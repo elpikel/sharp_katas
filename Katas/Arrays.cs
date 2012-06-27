824010cf426629d1d5337c43cc1aee8d8a3a7ce3
Arkadiusz Plichta <el.pikel@gmail.com>
2012-06-27 11:36:43 +0200

    array initialization

--- Katas/Arrays.cs
+++ Katas/Arrays.cs
@@ -5,9 +5,30 @@ namespace Katas
     public class Arrays
     {
         [Test]
-        public void CanCreateSngleDimensionArray()
+        public void InitializationOfDifferentTypesOfArrays()
         {
+            // Declare a single-dimensional array 
+            var array1 = new int[5];
 
+            Assert.AreEqual(5, array1.Length);
+
+            // Declare and set array element values
+            var array2 = new int[] { 1, 3, 5, 7, 9 };
+
+            // Alternative syntax
+            int[] array3 = { 1, 2, 3, 4, 5, 6 };
+
+            // Declare a two dimensional array
+            var multiDimensionalArray1 = new int[2, 3];
+
+            // Declare and set array element values
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            // Declare a jagged array
+            var jaggedArray = new int[6][];
+
+            // Set the values of the first array in the jagged array structure
+            jaggedArray[0] = new[] { 1, 2, 3, 4 };
         }
     }
 }
